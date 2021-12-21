703f52608529d00146e1c5b9e06752fe61064dbe
gavinerh <gavinerh@gmail.com>
2021-12-21 18:20:31 +0800

    added pagination example for list in functional programming

--- FunctionalProgramming/src/java1/Main.java
+++ FunctionalProgramming/src/java1/Main.java
@@ -3,23 +3,38 @@ package java1;
 import java.util.List;
 import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
     public  static void main(String[] args){
-        List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+
+        List<Integer> nums = limitList(3);
+        for(Integer i : nums){
+            System.out.println(i);
+        }
+
 //        functionalProgram(numbers);
-        BinaryOperator<Integer> sumBinaryOperator = Integer::sum;
-        BinaryOperator<Integer> sumBinaryOperator2 = new BinaryOperator<Integer>(){
-            @Override
-            public Integer apply(Integer integer, Integer integer2) {
-                return integer + integer2;
-            }
-        };
-        int sum = numbers.stream().reduce(0, Integer::sum);
+//        BinaryOperator<Integer> sumBinaryOperator = Integer::sum;
+//        BinaryOperator<Integer> sumBinaryOperator2 = new BinaryOperator<Integer>(){
+//            @Override
+//            public Integer apply(Integer integer, Integer integer2) {
+//                return integer + integer2;
+//            }
+//        };
+//        int sum = numbers.stream().reduce(0, Integer::sum);
+//
+//        Predicate<Integer> addPredicate = x -> x % 2 == 0;
+//
+//        filterAndPrint(numbers, addPredicate);
+    }
 
-        Predicate<Integer> addPredicate = x -> x % 2 == 0;
+    public static List<Integer> limitList(int num){
+        List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
 
-        filterAndPrint(numbers, addPredicate);
+        List<Integer> list = numbers.stream()
+                .skip(num*3).limit(3)
+                .collect(Collectors.toList());
+        return list;
     }
 
     private static void filterAndPrint(List<Integer> numbers, Predicate<Integer> addPredicate) {
