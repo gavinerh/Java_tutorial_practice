7268c2c2284219f6aedc7a6930abb88afd80970b
gavin erh <gavinerh@gmail.com>
2022-05-10 13:42:26 +0800

    made changes

--- queryNewLogs/query.py
+++ queryNewLogs/query.py
@@ -2,6 +2,7 @@ import requests
 import datetime
 import pandas as pd
 import json
+import re
 
 # Query elasticsearch api
 def search(uri, timeNow, earlierTime, searchQuery):
@@ -32,23 +33,56 @@ def search(uri, timeNow, earlierTime, searchQuery):
     return results
 
 # write response into csv file
-def exportToCSV(res, title, csvfile):
+def exportToCSV(res,  csvfile):
     with open(csvfile, 'w') as f:
-        f.write(title)
-        f.write('\n')
         for s in res:
             f.write(s)
             f.write('\n')
         f.close()
     return
 
-# Generate String from response object
+# create classification buckets
+def createBuckets(responseStr):
+    # extract the folder and file name
+    arr = responseStr.split(' ')
+    folder = arr[0]
+    file = arr[1]
+    return "/home/docker/graylog/logfiles/" + folder + "/" + file
+
+
+# populate response logs in dictionary
+def populateDictionay(dict, response, filename):
+    if (filename not in dict):
+        dict[filename] = []
+    else:
+        dict[filename] = response
+    return dict
+
+# sort response into individual buckets
 def createCSVString(response):
     index = 0
+    # contain the filenames as the key
+    myDict = {}
     while index < len(response):
-        response[index] = response[index]['_source']['message'].replace(': ', ',')
+        response[index] = response[index]['_source']['message']
+        # remove process id in []
+        response[index] = re.sub('\[(.*?)\]', '', response[index])
+        # remove process id in ()
+        response[index] = re.sub('\(.[0-9]*?\)', '', response[index])
+        filename = createBuckets(response[index])
+        myDict = populateDictionay(myDict, response[index], filename)
         index+=1
-    return response
+    return myDict
+
+def dumpToFiles(mydict):
+    for k in mydict.keys():
+        # loop through keys
+        with open(k, 'w') as f:
+            for s in mydict[k]:
+                f.write(s)
+                f.write('\n')
+        f.close()
+    return
 
 #search elasticsearch for jmpadm related fields
 uri = "http://localhost:9200/graylog_0/_search"
@@ -58,11 +92,13 @@ searchQuery = "jmpadm"
 response = search(uri, timeNow, earlierTime, searchQuery)
 print("Returned response length: " + str(len(response['hits']['hits'])))
 
-# create csv string for jmpadm from response
-response = createCSVString(response['hits']['hits'])
-title = "\"Source\",\"Message\",\"Remarks\""
+# preprocess response string and generate dict of filenames as key
+mydict = createCSVString(response['hits']['hits'])
+
+# generate files for the earlier classification
+dumpToFiles(mydict)
 
 # save the query string into csv file
 csvfile = "C:\\Users\\gavin\\OneDrive\\Desktop\\graylog\\logs\\jmpadm.csv"
-exportToCSV(response, title, csvfile)
+exportToCSV(response[:-1], csvfile)
 print("csv created for jmpadm logs")
